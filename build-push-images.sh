89006057a7247d5ad0f3964e3952eb71b4e35d6a
Alex Guidi <aguidi@redhat.com>
2024-05-10 17:16:45 +0200

    pushing images by platform

--- build-push-images.sh
+++ build-push-images.sh
@@ -10,5 +10,7 @@ for OS in "${OS_LIST[@]}"; do
   for ARCH in "${ARCH_LIST[@]}"; do
     echo "Building image for OS: $OS, ARCH: $ARCH"
     podman build --rm --build-arg MY_OS=${OS} --build-arg MY_ARCH=${ARCH} -t ${REGISTRY}/${USER}/${REPOSITORY}:${OS}_${ARCH} .
+    echo "Pushing image for OS: $OS, ARCH: $ARCH"
+    podman push ${REGISTRY}/${USER}/${REPOSITORY}:${OS}_${ARCH}
   done
 done
